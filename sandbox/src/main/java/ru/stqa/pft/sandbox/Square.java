463986f6c6bdb7d751352e325caebf5bcd03da3e
JulMann <julia.mannanova@gmail.com>
2020-02-23 17:37:23 +0300

    Созданы классы Square и Rectangle

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by user on 23.02.2020.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+
+  }
+
+}
